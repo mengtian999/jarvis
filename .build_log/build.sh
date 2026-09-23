8bfa1a04b5b008604b9d4087568965ddfb6209ae
mengtian999 <138861493+mengtian999@users.noreply.github.com>
2026-09-24 00:47:38 +0800

    Delete .build_log directory

--- .build_log/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export JAVA_HOME="$(cygpath -u 'C:\Program Files\Microsoft\jdk-17.0.20.8-hotspot')"
-export PATH="$JAVA_HOME/bin:$PATH"
-cd "D:/Code/IM/OpenMinis/OpenMinis-main/src/android" || exit 1
-./gradlew :app:assembleDebug --console=plain --no-daemon 2>&1
--- .build_log/dump_text.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import re
-import io
-p = r'D:\Code\IM\OpenMinis\OpenMinis-main\.build_log\r14.xml'
-d = io.open(p, encoding='utf-8', errors='replace').read()
-for m in re.findall(r'text="([^"]*)"[^>]*?class="android.widget.TextView"', d):
-    if m.strip():
-        print(m)
